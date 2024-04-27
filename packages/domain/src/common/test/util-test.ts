e4d578f1542e18249a94d2821dc8a881ada1cb17
Jose Guillermo <jguillermo@outlook.com>
2024-04-27 16:29:01 -0500

    test: update test validate corection

--- packages/domain/src/common/test/util-test.ts
+++ packages/domain/src/common/test/util-test.ts
@@ -1,28 +1,23 @@
 export function utilTest(vo: any, objectList: { [P: string]: any[] }) {
     for (const property in objectList) {
         objectList[property].forEach((value) => {
-            it(`validates ${vo.name} toEqual`, () => {
-                let type;
-                let result;
-                let input;
-                let expectValue;
+            let type;
+            let result;
+            let input = Array.isArray(value) && value.length === 2? value[0]: '';
+            let expectValue= Array.isArray(value) && value.length === 2? value[1]: value;
+            it(`validates (new ${vo.name}(${JSON.stringify(input)})).${property}() toEqual ${JSON.stringify(expectValue)}`, () => {
                 try {
                     if (Array.isArray(value) && value.length === 2) {
-                        input = value[0];
                         type = new vo(input);
                         result = type[property];
-                        expectValue = value[1];
                         expect(result).toEqual(expectValue);
                     } else {
-                        input = '';
                         type = new vo();
                         result = type[property];
-                        expectValue = value;
                         expect(result).toEqual(expectValue);
                     }
                 } catch (error) {
-                    throw new Error(`${vo.name}(${JSON.stringify(input)}).${property}() Expected: ${JSON.stringify(expectValue)}, but return: ${JSON.stringify(result)}.`);
-
+                    throw new Error(`(new ${vo.name}(${JSON.stringify(input)})).${property}() Expected: ${JSON.stringify(expectValue)}, but return: ${JSON.stringify(result)}.`);
                 }
             });
 
