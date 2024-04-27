25ae64e648c405480f315a9be366cd0051d3537a
Jose Guillermo <jguillermo@outlook.com>
2024-04-26 21:33:37 -0500

    test: feat refactor test

--- packages/domain/src/type/string/string-type.spec.ts
+++ packages/domain/src/type/string/string-type.spec.ts
@@ -38,48 +38,59 @@ describe('String Type', () => {
             [-1.1, '-1.1'],
         ]);
     });
-
-    describe('isNull', () => {
-        it('null', () => {
-            expectToEqual(StringTypeImp, 'isNull', [
-                true,
-                [undefined, true],
-                [null, true],
-                [0, false],
-                [0.1, false],
-                [1, false],
-                [1.1, false],
-                [false, false],
-                [true, false],
-                ['abc', false],
-                ['0', false],
-                ['1', false],
-            ]);
-        });
+    it('isNull', () => {
+        expectToEqual(StringTypeImp, 'isNull', [
+            true,
+            [undefined, true],
+            [null, true],
+            [0, false],
+            [0.1, false],
+            [1, false],
+            [1.1, false],
+            [false, false],
+            [true, false],
+            ['abc', false],
+            ['0', false],
+            ['', false],
+            ['1', false],
+        ]);
+    });
+    it('isEmpty', () => {
+        expectToEqual(StringTypeImp, 'isEmpty', [
+            ['', true],
+            true,
+            [null, true],
+            [undefined, true],
+            ['0', false],
+            ['false', false],
+            ['abc123', false],
+            ['áéíóú', false],
+            [0, false],
+            [0.1, false],
+            [1, false],
+            [1.1, false],
+            [false, false],
+            [true, false],
+        ]);
     });
 
-    describe('isEmpty', () => {
-        it('Empty string', () => {
-            type = new StringTypeImp('');
-            expect(type.isEmpty).toEqual(true);
-        });
-        it('no param', () => {
-            type = new StringTypeImp();
-            expect(type.isEmpty).toEqual(true);
-        });
-        it('null', () => {
-            type = new StringTypeImp(null);
-            expect(type.isEmpty).toEqual(true);
-        });
-        it('0', () => {
-            type = new StringTypeImp('0');
-            expect(type.isEmpty).toEqual(false);
-        });
-        it('false', () => {
-            type = new StringTypeImp('false');
-            expect(type.isEmpty).toEqual(false);
-        });
+    it('toString', () => {
+        expectToEqual(StringTypeImp, 'toString', [
+            ['', ''],
+            '',
+            [null, ''],
+            ['abc123', 'abc123'],
+            ['áéíóú', 'áéíóú'],
+            [0, '0'],
+            [0.1, '0.1'],
+            [1, '1'],
+            [1.1, '1.1'],
+            [false, 'false'],
+            [true, 'true'],
+        ]);
     });
+
+
     describe('length', () => {
         it('Empty string', () => {
             type = new StringTypeImp('');
@@ -110,36 +121,7 @@ describe('String Type', () => {
             expect(type.length).toEqual(5);
         });
     });
-    describe('toString', () => {
-        it('Empty string', () => {
-            type = new StringTypeImp('');
-            expect(type.toString).toEqual('');
-        });
-        it('no param', () => {
-            type = new StringTypeImp();
-            expect(type.toString).toEqual('');
-        });
-        it('null', () => {
-            type = new StringTypeImp(null);
-            expect(type.toString).toEqual('');
-        });
-        it('alpha', () => {
-            type = new StringTypeImp('abcdef');
-            expect(type.toString).toEqual('abcdef');
-        });
-        it('special alpha', () => {
-            type = new StringTypeImp('áéíóúñ');
-            expect(type.toString).toEqual('áéíóúñ');
-        });
-        it('0', () => {
-            type = new StringTypeImp('0');
-            expect(type.toString).toEqual('0');
-        });
-        it('false', () => {
-            type = new StringTypeImp('false');
-            expect(type.toString).toEqual('false');
-        });
-    });
+
     describe('isLength', () => {
         describe('min', () => {
             it('Empty string', () => {
