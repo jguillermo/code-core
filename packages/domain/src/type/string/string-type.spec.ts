796d74a0a29f8ca8217f578e76b86eb3a1448523
Jose Guillermo <jguillermo@outlook.com>
2024-04-26 22:52:27 -0500

    test: feat refactor test

--- packages/domain/src/type/string/string-type.spec.ts
+++ packages/domain/src/type/string/string-type.spec.ts
@@ -1,17 +1,5 @@
 import {StringTypeImp} from './';
-
-function expectToEqual(vo: any, property: string, values: any[]) {
-    values.forEach((value) => {
-        let type;
-        if (Array.isArray(value) && value.length === 2) {
-            type = new vo(value[0]);
-            expect(type[property]).toEqual(value[1]);
-        } else {
-            type = new vo();
-            expect(type[property]).toEqual(value);
-        }
-    });
-}
+import {expectToEqual} from "../../common/test/expect-to-equal";
 
 describe('String Type', () => {
     let type: StringTypeImp;
