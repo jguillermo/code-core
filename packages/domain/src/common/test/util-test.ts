c322bca65b0142939d94b690ba8fa6ab86ca4fca
Jose Guillermo <jguillermo@outlook.com>
2024-05-05 21:03:03 -0500

    feat: refactor util test

--- packages/domain/src/common/test/util-test.ts
+++ packages/domain/src/common/test/util-test.ts
@@ -1,5 +1,34 @@
 import {isString} from "class-validator";
 
+export function testValidation({validator, valid, invalid}) {
+
+  valid.forEach(input => {
+    it(`validate expected: true ${validator.name}(${universalToString(input)})`, () => {
+      let result;
+      try {
+        result = validator(input);
+        expect(result).toBeTruthy();
+      } catch (error) {
+        throw new Error(`${validator.name}(${universalToString(input)}). Expected: true, but return: ${result}.`);
+      }
+    });
+  });
+
+  invalid.forEach(input => {
+    it(`validate expected: false ${validator.name}(${universalToString(input)})`, () => {
+      let result;
+      try {
+        result = validator(input);
+        expect(result).toBeFalsy();
+      } catch (error) {
+        // Si falla la aserción, lanza un nuevo error con más información
+        throw new Error(`Error in ${validator.name}(${universalToString(input)}). Expected: false, but return: ${result}.`);
+
+      }
+    });
+  });
+}
+
 export function universalToString(value) {
   if (value === null) {
     return 'null';
@@ -30,94 +59,54 @@ export function universalToString(value) {
   return String(value);
 }
 
-//todo refactorizar la funcion para que sea mas sencilo dem mabener
+export function titleGenerate(objectName: string, objectItem: any, result: any, property: string | null = null): string {
+  const hastTwoValues = Array.isArray(objectItem) && objectItem.length === 2;
+  const input = hastTwoValues ? objectItem[0] : '';
+  const expectValue = hastTwoValues ? objectItem[1] : objectItem;
+  const txtExpectValue = isString(expectValue) ? `'${expectValue}'` : universalToString(expectValue);
+  let txtInput = isString(input) ? `'${input}'` : universalToString(input);
+  if (!Array.isArray(objectItem)) {
+    txtInput = '';
+  }
+  const titleObject = property ? `(new ${objectName}(${txtInput})).${property}()` : `${objectName}(${txtInput})`;
+  if (result) {
+    return `${titleObject} Expected: ${txtExpectValue}, but return: ${universalToString(result)}`;
+  } else {
+    return `validate ${titleObject} toEqual ${txtExpectValue}`;
+  }
+}
+
+function validateFunction(vo: any, objectItem: any, property = null) {
+  const hastTwoValues = Array.isArray(objectItem) && objectItem.length === 2;
+  let result;
+  let input = hastTwoValues ? objectItem[0] : '';
+  let expectValue = hastTwoValues ? objectItem[1] : objectItem;
+  it(titleGenerate(vo.name, objectItem, property), () => {
+    try {
+      if (property) {
+        const type = hastTwoValues ? new vo(input) : new vo();
+        result = type[property];
+      } else {
+        result = hastTwoValues ? vo(input) : vo();
+      }
+      expect(result).toEqual(expectValue);
+    } catch (error) {
+      throw new Error(titleGenerate(vo.name, objectItem, result, property));
+    }
+  });
+}
+
 export function utilTestSpec(vo: any, objectList: any[] | { [P: string]: any[] }) {
   if (Array.isArray(objectList)) {
     objectList.forEach((value) => {
-      const hastTwoValues = Array.isArray(value) && value.length === 2;
-      let result;
-      let input = hastTwoValues ? value[0] : '';
-      let expectValue = hastTwoValues ? value[1] : value;
-      const txtExpectValue = isString(expectValue) ? `'${expectValue}'` : universalToString(expectValue);
-      let txtInput = isString(input) ? `'${input}'` : universalToString(input);
-      if (!Array.isArray(value)) {
-        txtInput = '';
-
-      }
-      it(`validate ${vo.name}(${txtInput}) toEqual ${txtExpectValue}`, () => {
-        try {
-          if (hastTwoValues) {
-            result = vo(input);
-            expect(result).toEqual(expectValue);
-          } else {
-            result = new vo();
-            expect(result).toEqual(expectValue);
-          }
-        } catch (error) {
-          throw new Error(`(${vo.name}(${txtInput})) Expected: ${txtExpectValue}, but return: ${universalToString(result)}.`);
-        }
-      });
+      validateFunction(vo, value);
     });
   } else {
     for (const property in objectList) {
       objectList[property].forEach((value) => {
-        let type;
-        let result;
-        let input = Array.isArray(value) && value.length === 2 ? value[0] : null;
-        let expectValue = Array.isArray(value) && value.length === 2 ? value[1] : value;
-        const txtExpectValue = isString(expectValue) ? `'${expectValue}'` : universalToString(expectValue);
-        let txtInput = isString(input) ? `'${input}'` : universalToString(input);
-        if (!Array.isArray(value)) {
-          txtInput = '';
-        }
-        it(`validate (new ${vo.name}(${txtInput})).${property}() toEqual ${txtExpectValue}`, () => {
-          try {
-            if (Array.isArray(value) && value.length === 2) {
-              type = new vo(input);
-              result = type[property];
-              expect(result).toEqual(expectValue);
-            } else {
-              type = new vo();
-              result = type[property];
-              expect(result).toEqual(expectValue);
-            }
-          } catch (error) {
-            throw new Error(`(new ${vo.name}(${txtInput})).${property}() Expected: ${txtExpectValue}, but return: ${universalToString(result)}.`);
-          }
-        });
-
+        validateFunction(vo, value, property);
       });
     }
   }
 
-}
-
-export function testValidation({validator, valid, invalid}) {
-
-  valid.forEach(input => {
-    it(`validate expected: true ${validator.name}(${universalToString(input)})`, () => {
-      let result;
-      try {
-        result = validator(input);
-        expect(result).toBeTruthy();
-      } catch (error) {
-        throw new Error(`${validator.name}(${universalToString(input)}). Expected: true, but return: ${result}.`);
-      }
-    });
-  });
-
-
-  invalid.forEach(input => {
-    it(`validate expected: false ${validator.name}(${universalToString(input)})`, () => {
-      let result;
-      try {
-        result = validator(input);
-        expect(result).toBeFalsy();
-      } catch (error) {
-        // Si falla la aserción, lanza un nuevo error con más información
-        throw new Error(`Error in ${validator.name}(${universalToString(input)}). Expected: false, but return: ${result}.`);
-
-      }
-    });
-  });
 }
