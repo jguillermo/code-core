ddb9ea67a9ff368e4a8ae5a347f53c881a9b7ace
Guillermo <jguillermo@outlook.com>
2024-07-08 08:02:17 -0500

    feat: add json decorator validator

--- packages/domain/src/type/json/abstract-json-type.ts
+++ packages/domain/src/type/json/abstract-json-type.ts
@@ -1,16 +1,15 @@
 import { AbstractType } from '../abstract-type';
-import { NumberValidator } from '../../validator';
 import { AddValidate } from '../../validator/decorator/type-validator';
-import { CanBeNumberValidator } from '../../validator/decorator/custom/can-be-number';
+import { JsonValidator } from '../../validator/json.validator';
+import { CanBeJson } from '../../validator/decorator/custom/can-be-json';
 
 export type JsonTypeValue = {};
 
-@AddValidate([{ validator: CanBeNumberValidator }])
+@AddValidate([{ validator: CanBeJson }])
 export class AbstractJsonType<T extends JsonTypeValue, R extends null | undefined = undefined> extends AbstractType<T, R> {
   protected filter(value: any): any {
-    if (NumberValidator.canBeNumber(value)) {
-      const number = Number(value);
-      return isNaN(number) ? NaN : number.valueOf();
+    if (JsonValidator.canBeJson(value) && typeof value === 'string') {
+      return JSON.parse(value);
     }
     return value;
   }
---
+++ packages/domain/src/validator/decorator/custom/can-be-json.ts
@@ -0,0 +1,21 @@
+import { registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator';
+import { JsonValidator } from '../../json.validator';
+
+export function CanBeJson(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'canBeJson',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: any) {
+          return JsonValidator.canBeJson(value);
+        },
+        defaultMessage(args: ValidationArguments) {
+          return `${args.property} must be a object or a valid JSON string.`;
+        },
+      },
+    });
+  };
+}
