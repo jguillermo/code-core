82cab7995ce72626fda6e45daec23aded227e03d
Guillermo <jguillermo@outlook.com>
2024-08-17 12:56:42 -0500

    refactor: add validatiosn strint

--- packages/domain/src/common/test/json-commpare.ts
+++ packages/domain/src/common/test/json-commpare.ts
@@ -16,10 +16,10 @@ export class JsonCompare {
     return this._differences;
   }
 
-  constructor(obj1: any, obj2: any, strict = false) {
+  constructor(data: any, reference: any, strict = false) {
     this.strict = strict;
     this._differences = [];
-    this.compareValues(obj1, obj2, '');
+    this.compareValues(data, reference, '');
   }
 
   private sortArray(arr: JsonArray): JsonArray {
@@ -52,13 +52,13 @@ export class JsonCompare {
     return sortedObj;
   }
 
-  private compareArrays(val1: JsonArray, val2: JsonArray, path: string) {
-    if (this.strict && val1.length !== val2.length) {
-      this._differences.push(`${path}: length of ${JSON.stringify(val1)} is not equal to length of ${JSON.stringify(val2)}`);
+  private compareArrays(data: JsonArray, reference: JsonArray, path: string) {
+    if (this.strict && data.length !== reference.length) {
+      this._differences.push(`${path}: length of ${JSON.stringify(data)} is not equal to length of ${JSON.stringify(reference)}`);
       return;
     }
-    for (let i = 0; i < val1.length; i++) {
-      this.compareValues(val1[i], val2[i], `${path}[${i}]`);
+    for (let i = 0; i < data.length; i++) {
+      this.compareValues(data[i], reference[i], `${path}[${i}]`);
     }
   }
 
@@ -69,17 +69,17 @@ export class JsonCompare {
     return `${path}.${afterPath}`;
   }
 
-  private compareObjectContent(obj1: JsonObject, obj2: JsonObject, path: string): string[] {
+  private compareObjectContent(data: JsonObject, obj2: JsonObject, path: string): string[] {
     const differences: string[] = [];
-    for (const key in obj1) {
-      if (Object.prototype.hasOwnProperty.call(obj1, key)) {
+    for (const key in data) {
+      if (Object.prototype.hasOwnProperty.call(data, key)) {
         if (!Object.prototype.hasOwnProperty.call(obj2, key)) {
           differences.push(`${this.processPath(path, key)}: key not found`);
         } else {
-          if (this.isObject(obj1[key])) {
-            differences.push(...this.compareObjectContent(obj1[key] as JsonObject, obj2[key] as JsonObject, `${this.processPath(path, key)}`));
-          } else if (obj1[key] !== obj2[key]) {
-            differences.push(`${this.processPath(path, key)}: ${JSON.stringify(obj1[key])} !== ${JSON.stringify(obj2[key])}`);
+          if (this.isObject(data[key])) {
+            differences.push(...this.compareObjectContent(data[key] as JsonObject, obj2[key] as JsonObject, `${this.processPath(path, key)}`));
+          } else if (data[key] !== obj2[key]) {
+            differences.push(`${this.processPath(path, key)}: ${JSON.stringify(data[key])} !== ${JSON.stringify(obj2[key])}`);
           }
         }
       }
@@ -87,47 +87,48 @@ export class JsonCompare {
     return differences;
   }
 
-  private compareObjects(val1: JsonObject, val2: JsonObject, path: string) {
-    for (const key in val1) {
-      if (Object.prototype.hasOwnProperty.call(val1, key)) {
-        if (!Object.prototype.hasOwnProperty.call(val2, key)) {
+  private compareObjects(data: JsonObject, reference: JsonObject, path: string) {
+    for (const key in data) {
+      if (Object.prototype.hasOwnProperty.call(data, key)) {
+        if (!Object.prototype.hasOwnProperty.call(reference, key)) {
           this._differences.push(`${this.processPath(path, key)}: key not found`);
         } else {
-          this.compareValues(val1[key], val2[key], `${this.processPath(path, key)}`);
+          this.compareValues(data[key], reference[key], `${this.processPath(path, key)}`);
         }
       }
     }
     if (this.strict) {
-      for (const key in val2) {
-        if (Object.prototype.hasOwnProperty.call(val2, key) && !Object.prototype.hasOwnProperty.call(val1, key)) {
+      for (const key in reference) {
+        if (Object.prototype.hasOwnProperty.call(reference, key) && !Object.prototype.hasOwnProperty.call(data, key)) {
           this._differences.push(`${this.processPath(path, key)}: extra key found`);
         }
       }
     }
   }
 
-  private compareValues(val1: JsonValue, val2: JsonValue, path: string) {
-    if (Array.isArray(val1)) {
-      if (!Array.isArray(val2)) {
-        this._differences.push(`${path}: ${JSON.stringify(val1)} is not an array`);
+  private compareValues(data: JsonValue, reference: JsonValue, path: string) {
+    if (Array.isArray(data)) {
+      if (!Array.isArray(reference)) {
+        this._differences.push(`${path}: ${JSON.stringify(data)} is not an array`);
       } else {
-        this.compareArrays(val1, val2, path);
+        this.compareArrays(data, reference, path);
       }
-    } else if (this.isObject(val1)) {
-      if (!this.isObject(val2)) {
-        this._differences.push(`${path}: ${JSON.stringify(val1)} is not an object`);
+    } else if (this.isObject(data)) {
+      if (!this.isObject(reference)) {
+        this._differences.push(`${path}: ${JSON.stringify(data)} is not an object`);
       } else {
-        this.compareObjects(val1 as JsonObject, val2 as JsonObject, path);
+        this.compareObjects(data as JsonObject, reference as JsonObject, path);
       }
     } else {
-      const isEquals = CompareValue.getInstance().compare(val1, val2);
+      const isEquals = CompareValue.getInstance().compare(data, reference);
       if (!isEquals) {
-        this._differences.push(`${path}: ${JSON.stringify(val2)} -> ${JSON.stringify(val1)}`);
+        path = path === '' ? path : `${path}: `;
+        this._differences.push(`${path}${JSON.stringify(reference)} -> ${JSON.stringify(data)}`);
       }
     }
   }
 
-  private isObject(val: JsonValue): boolean {
-    return typeof val === 'object' && val !== null;
+  private isObject(data: JsonValue): boolean {
+    return typeof data === 'object' && data !== null;
   }
 }
--- packages/domain/src/common/test/json-compare.spec.ts
+++ packages/domain/src/common/test/json-compare.spec.ts
@@ -1,47 +1,100 @@
 import { JsonCompare } from './json-commpare';
 import { universalToString } from '../utils/string/universal-to-string';
 
-function _(obj1: any, obj2: any, expectValues: string[] = []) {
-  const jsonCompare = new JsonCompare(obj1, obj2, false);
-  it(`compare: ${universalToString(obj1)} eq ${universalToString(obj2)}`, () => {
-    expect(expectValues).toEqual(jsonCompare.differences);
+function compareValues(data: any, reference: any, strict: boolean, errors: string[]): void {
+  const jsonCompare = new JsonCompare(data, reference, strict);
+  it(`compare strict[${strict}]: ${universalToString(data)} eq ${universalToString(reference)}`, () => {
+    expect(errors).toEqual(jsonCompare.differences);
   });
 }
 
+function s(data: any, reference: any, errors: string[] = []) {
+  compareValues(data, reference, true, errors);
+}
+
+function _(include: any, reference: any, errors: string[] = []) {
+  compareValues(include, reference, false, errors);
+}
+
 describe('JsonCompare', () => {
-  describe('primitives', () => {
-    describe('equals ', () => {
-      _(1, 1);
-      _(false, false);
-      _('1', '1');
-      _('text', 'text');
-      _({ a: '1', b: 1 }, { a: '1', b: 1 });
-      _({ a: 1, b: 1 }, { a: 1, b: 1 });
-      _([1], [1]);
-      _([1, 2], [1, 2]);
-      _([1, '2'], [1, '2']);
-      _([1, '2', true], [1, '2', true]);
+  describe.only('primitives', () => {
+    describe('strict ', () => {
+      describe('correct', () => {
+        s(1, 1);
+        s(false, false);
+        s('1', '1');
+        s('text', 'text');
+      });
+      describe('error', () => {
+        s(1, 2, ['2 -> 1']);
+        s(false, true, ['true -> false']);
+        s('1', '2', ['"2" -> "1"']);
+        s('text', 'text2', ['"text2" -> "text"']);
+      });
     });
-    describe('not equals ', () => {
-      _(1, 2, [': 2 -> 1']);
-      _('text', 'text2', [': "text2" -> "text"']);
+    describe('include', () => {
+      describe('correct', () => {
+        _(1, 1);
+        _(false, false);
+        _('1', '1');
+        _('text', 'text');
+      });
+      describe('error ', () => {
+        _(1, 2, ['2 -> 1']);
+        _(false, true, ['true -> false']);
+        _('1', '2', ['"2" -> "1"']);
+        _('text', 'text2', ['"text2" -> "text"']);
+      });
     });
   });
 
   describe('Objects', () => {
-    describe('equals ', () => {
-      _({ a: 1, b: 1 }, { a: 1, b: 1 });
-      _({ a: 1, b: 1, c: { ca: 1, cb: 1 } }, { a: 1, b: 1, c: { ca: 1, cb: 1 } });
+    describe('strict ', () => {
+      describe('correct', () => {
+        s({ a: '1', b: 1 }, { a: '1', b: 1 });
+        s({ a: 1, b: 1 }, { a: 1, b: 1 });
+        s({ a: 1, b: 1 }, { a: 1, b: 1 });
+        s({ a: 1, b: 1, c: { ca: 1, cb: 1 } }, { a: 1, b: 1, c: { ca: 1, cb: 1 } });
+      });
+      describe('error', () => {
+        s({ a: 1, b: 1 }, { a: 1, b: 2 }, ['b: 2 -> 1']);
+        s({ a: 1, b: 1, c: { ca: 1, cb: 1 } }, { a: 1, b: 1, c: { ca: 1, cb: 2 } }, ['c.cb: 2 -> 1']);
+        s({ a: 1, b: 1, c: { ca: 1, cb: 1 } }, { a: 1, b: 1, c: { ca: 1 } }, ['c.cb: key not found']);
+
+        s({ a: '1' }, { a: '1', b: 1 }, ['b: extra key found']);
+        s({ a: 1 }, { a: 1, b: 1 }, ['b: extra key found']);
+
+        s({ a: 1 }, { a: 1, b: 1 }, ['b: extra key found']); //key not found
+        s({ a: 1, c: { cb: 1 } }, { a: 1, b: 1, c: { ca: 1, cb: 1 } }, ['c.ca: extra key found', 'b: extra key found']); //'c.ca: key not found', 'b: key not found'
+      });
     });
-    describe('not equals ', () => {
-      _({ a: 1, b: 1 }, { a: 1, b: 2 }, ['b: 2 -> 1']);
-      _({ a: 1, b: 1, c: { ca: 1, cb: 1 } }, { a: 1, b: 1, c: { ca: 1, cb: 2 } }, ['c.cb: 2 -> 1']);
-      _({ a: 1, b: 1, c: { ca: 1, cb: 1 } }, { a: 1, b: 1, c: { ca: 1 } }, ['c.cb: key not found']);
+    describe('include', () => {
+      describe('correct', () => {
+        _({ a: 1 }, { a: 1, b: 1 });
+        _({ a: 1, c: { cb: 1 } }, { a: 1, b: 1, c: { ca: 1, cb: 1 } });
+        _({ a: '1' }, { a: '1', b: 1 });
+        _({ a: 1 }, { a: 1, b: 1 });
+      });
+      describe('error ', () => {
+        _({ a: 1, b: 1 }, { a: 1, b: 2 }, ['b: 2 -> 1']);
+        _({ a: 1, b: 1, c: { ca: 1, cb: 1 } }, { a: 1, b: 1, c: { ca: 1, cb: 2 } }, ['c.cb: 2 -> 1']);
+        _({ a: 1, b: 1, c: { ca: 1, cb: 1 } }, { a: 1, b: 1, c: { ca: 1 } }, ['c.cb: key not found']);
+      });
     });
   });
 
   describe('array Objects', () => {
     describe('equals ', () => {
+      s([1], [1]);
+      s([1, 2], [1, 2]);
+      s([1, '2'], [1, '2']);
+      s([1, '2', true], [1, '2', true]);
+
+      _([1], [1]);
+      _([1], [1, 2]);
+      _([1], [1, '2']);
+      _([1, '2'], [1, '2', true]);
+
       _([{ a: 1, b: 1 }], [{ a: 1, b: 1 }]);
       _([{ a: 1, b: 1, c: { ca: 1, cb: 1 } }], [{ a: 1, b: 1, c: { ca: 1, cb: 1 } }]);
       _([{ a: 1, b: 1, c: [{ ca: 1, cb: 1 }] }], [{ a: 1, b: 1, c: [{ ca: 1, cb: 1 }] }]);
@@ -52,6 +105,10 @@ describe('JsonCompare', () => {
       _([{ a: 1, b: 1, c: [{ ca: 1, cb: 1 }] }], [{ a: 1, b: 1, c: [{ ca: 1, cb: 2 }] }], ['[0].c[0].cb: 2 -> 1']);
       _([{ a: 1, b: 1, c: { ca: 1, cb: 1 } }], [{ a: 1, b: 1, c: { ca: 1 } }], ['[0].c.cb: key not found']);
       _([{ a: 1, b: 1, c: [{ ca: 1, cb: 1 }] }], [{ a: 1, b: 1, c: [{ ca: 1 }] }], ['[0].c[0].cb: key not found']);
+
+      s([1], [1, 2], [': length of [1] is not equal to length of [1,2]']);
+      s([1], [1, '2'], [': length of [1] is not equal to length of [1,"2"]']);
+      s([1, '2'], [1, '2', true], [': length of [1,"2"] is not equal to length of [1,"2",true]']);
     });
   });
 
