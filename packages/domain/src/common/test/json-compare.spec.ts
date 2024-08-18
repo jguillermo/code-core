a4e03ae65202944ab0dd71846b391c9e291d228a
Guillermo <jguillermo@outlook.com>
2024-08-18 02:15:32 -0500

    refactor: validate array

--- packages/domain/src/common/test/json-compare.spec.ts
+++ packages/domain/src/common/test/json-compare.spec.ts
@@ -124,71 +124,70 @@ describe('JsonCompare', () => {
     });
   });
 
-  //
-  // describe('values testing', () => {
-  //   _(
-  //     [
-  //       {
-  //         _id: 'UUID()',
-  //         name: 'Name',
-  //         description: 'Description',
-  //         studentCount: 12,
-  //         attemptType: 'test',
-  //         attemptCount: 0,
-  //         timeLimit: 20,
-  //         teacher: {
-  //           id: 'dc47ff49-90d0-4ae5-8c5d-7ff5ac8c5776',
-  //           role: 'teacher',
-  //           userName: 'teacher-demo',
-  //           email: 'teacher-demo@mail.com',
-  //           firstName: 'Teacher',
-  //           lastName: 'Demo',
-  //         },
-  //         scene: {
-  //           name: 'name',
-  //           description: 'description',
-  //           image: 'photo.png',
-  //           scene: { a: 1, b: 1, c: { ca: 1, cb: 1 } },
-  //         },
-  //         author: 'dc47ff49-90d0-4ae5-8c5d-7ff5ac8c5776',
-  //         tenant: '2504559e-6c28-4d78-8025-94f2714001c0',
-  //       },
-  //     ],
-  //     [
-  //       {
-  //         _id: '42e05ee0-ace6-4565-b86a-ffdb0590341e',
-  //         __v: 0,
-  //         attemptCount: 0,
-  //         attemptType: 'test',
-  //         createAt: { $date: 1723531231695 },
-  //         description: 'Description',
-  //         endAt: { $date: 1672876800000 },
-  //         name: 'Name',
-  //         scene: {
-  //           id: '4c8e015a-ae43-4c69-a349-badc7662fa29',
-  //           name: 'name',
-  //           description: 'description',
-  //           image: 'photo.png',
-  //           scene: { a: 1, b: 1, c: { ca: 1, cb: 1 } },
-  //         },
-  //         sceneId: '4c8e015a-ae43-4c69-a349-badc7662fa29',
-  //         startAt: { $date: 1672531200000 },
-  //         studentCount: 12,
-  //         teacher: {
-  //           id: 'dc47ff49-90d0-4ae5-8c5d-7ff5ac8c5776',
-  //           role: 'teacher',
-  //           userName: 'teacher-demo',
-  //           email: 'teacher-demo@mail.com',
-  //           firstName: 'Teacher',
-  //           lastName: 'Demo',
-  //           organizationId: '2504559e-6c28-4d78-8025-94f2714001c0',
-  //         },
-  //         timeLimit: 20,
-  //         tutorialMode: false,
-  //         author: 'dc47ff49-90d0-4ae5-8c5d-7ff5ac8c5776',
-  //         tenant: '2504559e-6c28-4d78-8025-94f2714001c0',
-  //       },
-  //     ],
-  //   );
-  // });
+  describe('values testing', () => {
+    _(
+      [
+        {
+          _id: 'UUID()',
+          name: 'Name',
+          description: 'Description',
+          studentCount: 12,
+          attemptType: 'test',
+          attemptCount: 0,
+          timeLimit: 20,
+          teacher: {
+            id: 'dc47ff49-90d0-4ae5-8c5d-7ff5ac8c5776',
+            role: 'teacher',
+            userName: 'teacher-demo',
+            email: 'teacher-demo@mail.com',
+            firstName: 'Teacher',
+            lastName: 'Demo',
+          },
+          scene: {
+            name: 'name',
+            description: 'description',
+            image: 'photo.png',
+            scene: { a: 1, b: 1, c: { ca: 1, cb: 1 } },
+          },
+          author: 'dc47ff49-90d0-4ae5-8c5d-7ff5ac8c5776',
+          tenant: '2504559e-6c28-4d78-8025-94f2714001c0',
+        },
+      ],
+      [
+        {
+          _id: '42e05ee0-ace6-4565-b86a-ffdb0590341e',
+          __v: 0,
+          attemptCount: 0,
+          attemptType: 'test',
+          createAt: { $date: 1723531231695 },
+          description: 'Description',
+          endAt: { $date: 1672876800000 },
+          name: 'Name',
+          scene: {
+            id: '4c8e015a-ae43-4c69-a349-badc7662fa29',
+            name: 'name',
+            description: 'description',
+            image: 'photo.png',
+            scene: { a: 1, b: 1, c: { ca: 1, cb: 1 } },
+          },
+          sceneId: '4c8e015a-ae43-4c69-a349-badc7662fa29',
+          startAt: { $date: 1672531200000 },
+          studentCount: 12,
+          teacher: {
+            id: 'dc47ff49-90d0-4ae5-8c5d-7ff5ac8c5776',
+            role: 'teacher',
+            userName: 'teacher-demo',
+            email: 'teacher-demo@mail.com',
+            firstName: 'Teacher',
+            lastName: 'Demo',
+            organizationId: '2504559e-6c28-4d78-8025-94f2714001c0',
+          },
+          timeLimit: 20,
+          tutorialMode: false,
+          author: 'dc47ff49-90d0-4ae5-8c5d-7ff5ac8c5776',
+          tenant: '2504559e-6c28-4d78-8025-94f2714001c0',
+        },
+      ],
+    );
+  });
 });
