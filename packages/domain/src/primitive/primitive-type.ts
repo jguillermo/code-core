e03774558ab96b78197ed841710e3e4561d4c926
Guillermo <jguillermo@outlook.com>
2024-08-28 22:57:04 -0500

    feat: refactor

--- packages/domain/src/primitive/primitive-type.ts
+++ packages/domain/src/primitive/primitive-type.ts
@@ -1,40 +1,28 @@
 import { AbstractBooleanType, AbstractDateType, AbstractEnumType, AbstractNumberType, AbstractStringType, AbstractUuidType, IdType } from '@code-core/domain';
 import { AbstractJsonType } from '../type/abstract-json-type';
 
+type Nullable<T> = T | null;
+
 type PrimitiveEnum<T> = T extends string ? string : T extends number ? number : never;
 
-export type PrimitiveType<T> =
-  T extends AbstractBooleanType<undefined>
-    ? boolean
-    : T extends AbstractBooleanType<null>
-      ? boolean | null
-      : T extends AbstractDateType<undefined>
-        ? Date
-        : T extends AbstractDateType<null>
-          ? Date | null
-          : T extends AbstractNumberType<undefined>
-            ? number
-            : T extends AbstractNumberType<null>
-              ? number | null
-              : T extends AbstractStringType<undefined>
-                ? string
-                : T extends AbstractStringType<null>
-                  ? string | null
-                  : T extends IdType
-                    ? string
-                    : T extends AbstractUuidType<undefined>
-                      ? string
-                      : T extends AbstractUuidType<null>
-                        ? string | null
-                        : T extends AbstractEnumType<infer U>
-                          ? PrimitiveEnum<U>
-                          : T extends AbstractEnumType<infer U, null>
-                            ? PrimitiveEnum<U> | null
-                            : T extends AbstractJsonType<infer U>
-                              ? object
-                              : T extends AbstractJsonType<infer U, null>
-                                ? object | null
-                                : never;
+type BooleanType<T> = T extends AbstractBooleanType<undefined> ? boolean : T extends AbstractBooleanType<null> ? Nullable<boolean> : never;
+
+type DateType<T> = T extends AbstractDateType<undefined> ? Date : T extends AbstractDateType<null> ? Nullable<Date> : never;
+
+type NumberType<T> = T extends AbstractNumberType<undefined> ? number : T extends AbstractNumberType<null> ? Nullable<number> : never;
+
+type StringType<T> = T extends AbstractStringType<undefined> ? string : T extends AbstractStringType<null> ? Nullable<string> : never;
+
+type UuidType<T> = T extends AbstractUuidType<undefined> ? string : T extends AbstractUuidType<null> ? Nullable<string> : never;
+
+type EnumType<T> = T extends AbstractEnumType<infer U> ? PrimitiveEnum<U> : T extends AbstractEnumType<infer U, null> ? Nullable<PrimitiveEnum<U>> : never;
+
+type JsonType<T> = T extends AbstractJsonType<infer U> ? object : T extends AbstractJsonType<infer U, null> ? Nullable<object> : never;
+
+type IdTypePrimitive<T> = T extends IdType ? string : never;
+
+export type PrimitiveType<T> = BooleanType<T> | DateType<T> | NumberType<T> | StringType<T> | UuidType<T> | EnumType<T> | JsonType<T> | IdTypePrimitive<T> | never;
+
 
 // export type PrimitiveType<T> = T extends PrimitiveTypes
 //   ? T
