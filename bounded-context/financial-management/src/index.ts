c70f76d081a3a67b532b19a3c8523510ad14d34c
Guillermo <jguillermo@outlook.com>
2024-11-03 21:42:28 -0500

    feat: run build

--- bounded-context/financial-management/src/index.ts
+++ bounded-context/financial-management/src/index.ts
@@ -1,1 +1,7 @@
+import { AggregateRoot } from '@code-core/domain';
+
 export const NAME = 'financial-management';
+
+class Aggregate extends AggregateRoot {}
+
+console.log(new Aggregate().pullDomainEvents());
