47f688310a93905e360d80ebcc543bfec0a114d6
Guillermo <jguillermo@outlook.com>
2024-12-04 21:49:15 -0500

    feat: change data to types

--- bounded-context/financial-management/src/domain/account/account.repository.ts
+++ bounded-context/financial-management/src/domain/account/account.repository.ts
@@ -1,4 +1,6 @@
 import { Account } from './account';
+import { PrimitiveTypes } from '@code-core/domain';
+import { AccountTypes } from './account.types';
 
 export abstract class AccountRepository {
   abstract findById(accountId: string): Account | null;
@@ -8,4 +10,19 @@ export abstract class AccountRepository {
   abstract findAll(): Account[];
 
   abstract findLiabilities(): Account[]; // Para reportes financieros
+
+  protected toAggregate(data: PrimitiveTypes<AccountTypes>): Account {
+    const dataTypes = new AccountTypes(1, data);
+    return new Account(
+      dataTypes.id,
+      dataTypes.name,
+      dataTypes.type,
+      dataTypes.currency,
+      dataTypes.balance,
+      dataTypes.financialEntity,
+      dataTypes.number,
+      dataTypes.tags,
+      dataTypes.creationDate,
+    );
+  }
 }
