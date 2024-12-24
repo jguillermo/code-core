d5b097402774323c8e892bbb59d5d292d8e01d1b
Guillermo <jguillermo@outlook.com>
2024-12-24 06:41:06 -0500

    feat: add import data

--- project/api/src/bounded-context/financial-management/infrastructure/repository/AccountInMemoryRepository.ts
+++ project/api/src/bounded-context/financial-management/infrastructure/repository/AccountInMemoryRepository.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { AccountId } from '@bounded-context/financial-management/dist/domain/account/types/account-id';
+import { IdType } from '@code-core/domain';
 
 @Injectable()
 export class AccountInMemoryRepository {
   async findAll() {
     return new AccountId(AccountId.random());
+  }
+
+  async findAll2() {
+    return new IdType(IdType.random());
   }
 }
