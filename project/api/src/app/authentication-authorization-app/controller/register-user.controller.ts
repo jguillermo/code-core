ad4470e2b27aafa1ceeab628d2497063beeff59f
Guillermo <jguillermo@outlook.com>
2025-02-23 20:41:11 -0500

    feat: add doc

--- project/api/src/app/authentication-authorization-app/controller/register-user.controller.ts
+++ project/api/src/app/authentication-authorization-app/controller/register-user.controller.ts
@@ -1,14 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import {
-  UserRegister,
-  UserRegisterDto,
-} from '@bounded-context/authentication-authorization';
+import { UserRegister } from '@bounded-context/authentication-authorization';
+import { CreateUserDto } from '../create-user.dto';
 
 @Controller('register-user')
 export class RegisterUserController {
   constructor(private readonly userRegister: UserRegister) {}
   @Post()
-  async registerUser(@Body() dto: UserRegisterDto) {
-    await this.userRegister.execute(dto);
+  async registerUser(@Body() dto: CreateUserDto) {
+    await this.userRegister.execute(dto as any);
   }
 }
--- project/api/src/main.ts
+++ project/api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,15 @@ async function bootstrap() {
       },
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
 
   await app.listen(3000);
 }
